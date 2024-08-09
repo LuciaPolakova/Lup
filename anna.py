e03d29bc9be3c755bb2e2e0d2d85a2fcf4e2415c
lucia <lucia.polakova@seznam.cz>
2024-08-09 20:29:13 +0200

    commit

---
+++ anna.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read_excel(r"C:\Users\Dell\Desktop\Ukol_Specialista_zpracovani_dat_v4 (1).xlsx")
+print(df)
---
+++ main.py
@@ -0,0 +1,10 @@
+print (2)
+
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} day are {num_of_days * calculation_to_units} {name_of_unit}")
+    print("All good!")
+
+days_to_units(35)
